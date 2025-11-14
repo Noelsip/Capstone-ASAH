a2b6e836d73f870e3fd60a1130b6288007cea0dd
akmalfay <faizagiputra.works@gmail.com>
2025-11-14 18:53:41 +0700

    express and docker setup

---
+++ backend/src/server.js
@@ -0,0 +1,21 @@
+import express from 'express';
+
+const app = express();
+
+// Env port or 3000
+const port = process.env.PORT || 3000;
+
+// Root route
+app.get('/', (req, res) => {
+  res.send('Hello World! Ini adalah BE Predictive Maintenance Copilot.');
+});
+
+// Health route
+app.get('/health', (req, res) => {
+  res.status(200).json({ status: 'UP' });
+});
+
+// Start server
+app.listen(port, () => {
+  console.log(`Server BE berjalan di http://localhost:${port}`);
+});
